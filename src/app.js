1d6b39e67be4688a7ef9a7657ce865b418183bdc
Mars <martinianol@outlook.com>
2021-11-26 12:35:10 -0300

    partials added

--- src/app.js
+++ src/app.js
@@ -1,42 +1,49 @@
-const express = require('express') // Require express module
-const app = express() //Declare const app where we will be using all express methods.
-
-
-//Setting route of statics resources (css, js, etc.)//
-const path = require('path')
-publicPath = path.join(__dirname, '../public')
-app.use(express.static(publicPath))
-
-
-//Require and implementing cors so we can use APIs and browser does not block the request//
+const express = require('express')
 const cors = require('cors')
-app.use(cors())
+const methodOverride = require('method-override')
+const path = require('path')
 
-//Setting EJS engine and views folder//
+const mainRoutes = require('./routes/mainRoutes')
+
+
+/**
+ * Constants
+ */
+
+const port = process.env.PORT || 3000
+const publicPath = path.join(__dirname, '../public')
+
+/**
+ * Setup
+ */
+
+const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-//Setting express so we can work with data sent from forms//
-app.use(express.urlencoded({ extended: false })) // with this line we can use body data from form
-app.use(express.json())
+/**
+ * Middlewares
+ */
 
-//The following 2 lines are used in case PUT and DELETE requests are needed//
-const methodOverride = require('method-override')
+app.use(cors())
 app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static(publicPath))
 
-//Server settings
-const port = process.env.PORT || 3000// use port 3000 unless there exists a preconfigured port (e.g. Heroku)//
+/**
+ * Routes
+ */
+
+app.use('/', mainRoutes)
+/* app.use((req, res, next) => {
+  res.status(404).render('404.ejs')
+}) */
+
+/**
+ * 
+ */
+
 app.listen(port, () =>
   console.log('Server up and running at ' + port + '\n' + 'Torre is the best place to work!!!')
 )
-
-
-//Setting Routes
-const mainRoutes = require('./routes/mainRoutes')
-app.use('/', mainRoutes)
-
-app.use('/', (req, res, next) => res.render('index.ejs'))
-// Rooute 404
-app.use((req, res, next) => {
-  res.status(404).render('404.ejs')
-})
--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -7,10 +7,12 @@ const controller = {
   },
 
   search: async (req, res) => {
-    let userToFind = req.query.keyword
-    let url = 'https://bio.torre.co/api/bios/'
+    let userToFind = req.query.keyword;
+    let url = 'https://bio.torre.co/api/bios/';
+
     const response = await fetch(`${url}${userToFind}`);
     const user = await response.json();
+
     res.render('profile.ejs', { user })
   }
 }
